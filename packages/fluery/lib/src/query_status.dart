50c02a200744dc514adc7a64ea7c8385fbf9dee9
Jaesung Hong <jezsung@gmail.com>
2023-04-04 19:13:10 -0500

    feat(fluery): add extension getters for QueryStatus

--- packages/fluery/lib/src/query_status.dart
+++ packages/fluery/lib/src/query_status.dart
@@ -5,3 +5,17 @@ enum QueryStatus {
   success,
   failure,
 }
+
+extension QueryStatusExtension on QueryStatus {
+  bool get isIdle => this == QueryStatus.idle;
+
+  bool get isFetching => this == QueryStatus.fetching;
+
+  bool get isRetrying => this == QueryStatus.retrying;
+
+  bool get isLoading => isFetching || isRetrying;
+
+  bool get isSuccess => this == QueryStatus.success;
+
+  bool get isFailure => this == QueryStatus.failure;
+}
