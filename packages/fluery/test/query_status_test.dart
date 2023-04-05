f163ca83f2831ffd14b81742e34886df03a83c86
Jaesung Hong <jezsung@gmail.com>
2023-04-04 19:13:51 -0500

    test(fluery): add tests for QueryStatusExtension

---
+++ packages/fluery/test/query_status_test.dart
@@ -0,0 +1,31 @@
+import 'package:fluery/fluery.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('QueryStatusExtension', () {
+    test('isIdle returns true for idle status', () {
+      expect(QueryStatus.idle.isIdle, isTrue);
+    });
+
+    test('isFetching returns true for fetching status', () {
+      expect(QueryStatus.fetching.isFetching, isTrue);
+    });
+
+    test('isRetrying returns true for retrying status', () {
+      expect(QueryStatus.retrying.isRetrying, isTrue);
+    });
+
+    test('isLoading returns true for fetching and retrying statuses', () {
+      expect(QueryStatus.fetching.isLoading, isTrue);
+      expect(QueryStatus.retrying.isLoading, isTrue);
+    });
+
+    test('isSuccess returns true for success status', () {
+      expect(QueryStatus.success.isSuccess, isTrue);
+    });
+
+    test('isFailure returns true for failure status', () {
+      expect(QueryStatus.failure.isFailure, isTrue);
+    });
+  });
+}
