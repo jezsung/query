29e133fb7a55adb89eec30d3610af6d0e3b1baae
Jaesung Hong <jezsung@gmail.com>
2023-04-05 22:38:26 -0500

    test(fluery): add failure test

--- packages/fluery/test/query_builder_test.dart
+++ packages/fluery/test/query_builder_test.dart
@@ -82,4 +82,34 @@ void main() {
       expect(find.text('error: null'), findsOneWidget);
     },
   );
+
+  testWidgets(
+    'should end with a failure status',
+    (tester) async {
+      await tester.pumpWithQueryClientProvider(
+        QueryBuilder<String>(
+          id: 'id',
+          fetcher: (id) async {
+            await Future.delayed(const Duration(seconds: 3));
+            throw 'error';
+          },
+          builder: (context, state, child) {
+            return Column(
+              children: [
+                Text('status: ${state.status.name}'),
+                Text('data: ${state.data}'),
+                Text('error: ${state.error}'),
+              ],
+            );
+          },
+        ),
+      );
+
+      await tester.pump(const Duration(seconds: 4));
+
+      expect(find.text('status: failure'), findsOneWidget);
+      expect(find.text('data: null'), findsOneWidget);
+      expect(find.text('error: error'), findsOneWidget);
+    },
+  );
 }
