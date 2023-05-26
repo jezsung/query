3f0aa66ebd7605e69fe465c7ce955134e29e3845
Jaesung Hong <jezsung@gmail.com>
2023-05-26 11:13:24 +0900

    refactor(query): encapsulate properties

--- packages/query/lib/src/query_observer.dart
+++ packages/query/lib/src/query_observer.dart
@@ -1,25 +1,29 @@
 part of 'query.dart';
 
 class QueryObserver<T> implements Observer<QueryState<T>> {
-  QueryObserver(this.state);
+  QueryObserver(this._state);
 
-  Query<T>? query;
-  QueryState<T> state;
+  Query<T>? _query;
+  QueryState<T> _state;
+
+  Query<T>? get query => _query;
+
+  QueryState<T> get state => _state;
 
   @override
   void onNotified(QueryState<T> state) {
-    this.state = state;
+    this._state = state;
   }
 
   @override
   void onAdded(covariant Query<T> query) {
-    this.query = query;
+    this._query = query;
   }
 
   @override
   void onRemoved(covariant Query<T> query) {
-    if (this.query == query) {
-      this.query = null;
+    if (this._query == query) {
+      this._query = null;
     }
   }
 }
