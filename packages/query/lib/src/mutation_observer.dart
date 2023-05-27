0540731ac4f0be59d5ad72fe2dd44f9d358c8b5c
Jaesung Hong <jaesunghong@Jaesungs-Mac-mini.local>
2023-05-27 12:07:10 +0900

    chore(query): encapsulate state property

--- packages/query/lib/src/mutation_observer.dart
+++ packages/query/lib/src/mutation_observer.dart
@@ -1,25 +1,27 @@
 part of 'mutation.dart';
 
 class MutationObserver<T, P> implements Observer<MutationState<T>> {
-  MutationObserver(this.state);
+  MutationObserver(this._state);
 
-  Mutation<T, P>? mutation;
-  MutationState<T> state;
+  Mutation<T, P>? _mutation;
+  MutationState<T> _state;
+
+  MutationState<T> get state => _state;
 
   @override
   void onNotified(MutationState<T> state) {
-    this.state = state;
+    this._state = state;
   }
 
   @override
   void onAdded(covariant Mutation<T, P> mutatoin) {
-    this.mutation = mutation;
+    this._mutation = _mutation;
   }
 
   @override
   void onRemoved(covariant Mutation<T, P> mutatoin) {
-    if (this.mutation == mutatoin) {
-      this.mutation = null;
+    if (this._mutation == mutatoin) {
+      this._mutation = null;
     }
   }
 }
