d2dd9375a4958e61262c65e07e20fa0056b40a31
Jaesung Hong <jezsung@gmail.com>
2023-05-30 19:03:03 +0900

    example(flutter_query): show QueryBuilder usage

--- packages/flutter_query/example/lib/main.dart
+++ packages/flutter_query/example/lib/main.dart
@@ -5,35 +5,120 @@ void main() {
   runApp(
     QueryClientProvider(
       create: (context) => QueryClient(),
-      child: const MyApp(),
+      child: const App(),
     ),
   );
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class App extends StatelessWidget {
+  const App({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Fluery Example',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      title: 'futter_query Example',
+      theme: ThemeData(primarySwatch: Colors.blue),
       home: const HomePage(),
     );
   }
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late final QueryController<Post> _postController;
+
+  @override
+  void initState() {
+    super.initState();
+    _postController = QueryController<Post>();
+  }
+
+  @override
+  void dispose() {
+    _postController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Placeholder(),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('flutter_query'),
+      ),
+      body: SafeArea(
+        child: QueryBuilder<Post>(
+          controller: _postController,
+          id: 'https://jsonplaceholder.typicode.com/posts/1',
+          fetcher: (uri) async {
+            await Future.delayed(const Duration(seconds: 3));
+            return const Post(
+              title: 'This is flutter_query example!',
+              body: 'It is really awesome!',
+            );
+          },
+          builder: (context, state, child) {
+            if (state.status.isFailure) {
+              return const Center(child: Text('Something went wrong!'));
+            }
+
+            if (!state.hasData) {
+              return const Center(child: CircularProgressIndicator());
+            }
+
+            final post = state.data!;
+            return Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    post.title,
+                    style: const TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(height: 16),
+                  Text(post.body),
+                  ElevatedButton(
+                    onPressed: state.status.isFetching
+                        ? null
+                        : () {
+                            _postController.refetch();
+                          },
+                    child: state.status.isFetching
+                        ? const SizedBox(
+                            width: 16,
+                            height: 16,
+                            child: CircularProgressIndicator(
+                              strokeWidth: 2,
+                              color: Colors.white,
+                            ),
+                          )
+                        : const Text('Refetch'),
+                  ),
+                ],
+              ),
+            );
+          },
+        ),
       ),
     );
   }
+}
+
+class Post {
+  const Post({
+    required this.title,
+    required this.body,
+  });
+
+  final String title;
+  final String body;
 }
