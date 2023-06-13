ada7646f070259200590a77f0083c9e2e413fecd
Jaesung Hong <jezsung@gmail.com>
2023-06-13 12:19:29 +0900

    chore(website): update logo

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -64,7 +64,7 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: 'Flutter Query',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',
