972f10df1c8d195cb72bd59c5940f8024f37bf15
Jaesung Hong <jezsung@gmail.com>
2023-06-15 23:11:55 +0900

    feat(simple): complete

---
+++ examples/simple/lib/repository.dart
@@ -0,0 +1,25 @@
+class Repository {
+  Repository({
+    required this.name,
+    required this.description,
+    required this.watcherCount,
+    required this.starCount,
+    required this.forkCount,
+  });
+
+  final String name;
+  final String description;
+  final int watcherCount;
+  final int starCount;
+  final int forkCount;
+
+  factory Repository.fromJson(Map<String, dynamic> json) {
+    return Repository(
+      name: json['name'],
+      description: json['description'],
+      watcherCount: json['subscribers_count'],
+      starCount: json['stargazers_count'],
+      forkCount: json['forks_count'],
+    );
+  }
+}
