a9544a4d2a8cd348da73eba45556593b67314dfb
Jaesung Hong <jezsung@gmail.com>
2023-06-18 17:07:37 +0900

    refactor(simple): reorder properties

--- examples/simple/lib/repository.dart
+++ examples/simple/lib/repository.dart
@@ -3,23 +3,23 @@ class Repository {
     required this.name,
     required this.description,
     required this.watcherCount,
-    required this.starCount,
     required this.forkCount,
+    required this.starCount,
   });
 
   final String name;
   final String description;
   final int watcherCount;
-  final int starCount;
   final int forkCount;
+  final int starCount;
 
   factory Repository.fromJson(Map<String, dynamic> json) {
     return Repository(
       name: json['name'],
       description: json['description'],
       watcherCount: json['subscribers_count'],
-      starCount: json['stargazers_count'],
       forkCount: json['forks_count'],
+      starCount: json['stargazers_count'],
     );
   }
 }
