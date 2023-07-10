984d9e876e41df86d436be356d90e94873666514
Jaesung Hong <jaesunghong@Jaesungs-Mac-mini.local>
2023-07-10 21:14:41 +0900

    chore(flutter_query): remove redundant imports

--- packages/flutter_query/test/use_query_test.dart
+++ packages/flutter_query/test/use_query_test.dart
@@ -2,8 +2,6 @@ import 'package:clock/clock.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_query/flutter_query.dart';
-import 'package:flutter_query/src/hooks/use_query.dart';
-import 'package:flutter_query/src/hooks/use_query_client.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 Widget withQueryClientProvider(Widget widget, [QueryClient? client]) {
