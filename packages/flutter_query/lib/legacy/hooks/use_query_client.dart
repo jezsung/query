8444aa563af2db99d915d4aea5ebb976bb69c6b0
Vice Phenek <phenek.github@outlook.com>
2025-12-04 10:42:59 +0100

    - remove legacy code

--- packages/flutter_query/lib/legacy/hooks/use_query_client.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter_hooks/flutter_hooks.dart';
-import '../query_client.dart';
-import 'package:provider/provider.dart';
-
-QueryClient useQueryClient() {
-  final context = useContext();
-  return context.watch<QueryClient>();
-}
