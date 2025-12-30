425f5736bd571249962d3bb67846204ab179edc0
Jaesung Hong <45475169+jezsung@users.noreply.github.com>
2025-12-30 19:19:59 +0900

    Customize navigation
    - Add logo
    - Link to documentation overview page when logo is clicked
    - Make navigation background transparent

--- docs-fumadocs/lib/layout.shared.tsx
+++ docs-fumadocs/lib/layout.shared.tsx
@@ -1,9 +1,21 @@
+import Image from 'next/image';
 import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
 
 export function baseOptions(): BaseLayoutProps {
   return {
     nav: {
-      title: 'My App',
+      title: <Logo />,
+      url: '/docs/overview',
+      transparentMode: 'always',
     },
   };
+}
+
+function Logo() {
+  return (
+    <span className='flex items-center gap-3'>
+      <Image src='/logo.svg' alt='Flutter Query' width={20} height={20} />
+      <p className='font-medium'>Flutter Query</p>
+    </span>
+  );
 }
