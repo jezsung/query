bc85a229653699efb9a16337a37a6b9105332ef4
Jaesung Hong <45475169+jezsung@users.noreply.github.com>
2025-12-30 15:45:18 +0900

    Set up eslint-config-prettier
    Turn off all ESLint rules that are unnecessary or might conflict with
    Prettier using eslint-config-prettier.

--- docs-fumadocs/eslint.config.mjs
+++ docs-fumadocs/eslint.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig, globalIgnores } from 'eslint/config';
 import nextVitals from 'eslint-config-next/core-web-vitals';
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
 
 const eslintConfig = defineConfig([
   ...nextVitals,
+  eslintConfigPrettier,
   globalIgnores([
     '.next/**',
     'out/**',
