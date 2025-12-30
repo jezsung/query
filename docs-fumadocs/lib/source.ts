17f3d998b2da6784eb09cd509abafa2836c6af15
Jaesung Hong <45475169+jezsung@users.noreply.github.com>
2025-12-30 21:00:11 +0900

    Configure icon option for loader
    Allow referencing Lucide icons by their type names.

--- docs-fumadocs/lib/source.ts
+++ docs-fumadocs/lib/source.ts
@@ -1,12 +1,21 @@
+import { createElement } from 'react';
 import { type InferPageType, loader } from 'fumadocs-core/source';
 import { lucideIconsPlugin } from 'fumadocs-core/source/lucide-icons';
 import { docs } from 'fumadocs-mdx:collections/server';
+import { icons } from 'lucide-react';
 
 // See https://fumadocs.dev/docs/headless/source-api for more info
 export const source = loader({
   baseUrl: '/docs',
   source: docs.toFumadocsSource(),
   plugins: [lucideIconsPlugin()],
+  icon(icon) {
+    if (!icon) return;
+
+    if (icon in icons) {
+      return createElement(icons[icon as keyof typeof icons]);
+    }
+  },
 });
 
 export function getPageImage(page: InferPageType<typeof source>) {
