c61084ddf98b00e0d5672af0848ae8e63c85e60a
Jaesung Hong <45475169+jezsung@users.noreply.github.com>
2026-01-01 11:04:54 +0900

    Update dependencies
    - Use new slugs API with file argument from loader function to allow
      per-file slugs customization
    - Drop permalinkPlugin, replaced by new slugs API

--- docs/lib/source.ts
+++ docs/lib/source.ts
@@ -1,36 +1,23 @@
 import { createElement } from 'react';
-import { type InferPageType, LoaderPlugin, loader } from 'fumadocs-core/source';
+import { type InferPageType, loader } from 'fumadocs-core/source';
 import { lucideIconsPlugin } from 'fumadocs-core/source/lucide-icons';
 import { docs } from 'fumadocs-mdx:collections/server';
 import { icons } from 'lucide-react';
-
-// Plugin to use frontmatter permalink if provided
-const permalinkPlugin: LoaderPlugin = {
-  transformStorage({ storage }) {
-    for (const path of storage.getFiles()) {
-      const file = storage.read(path);
-      if (!file || file.format !== 'page') continue;
-
-      // Check if frontmatter has custom permalink
-      const { permalink } = file.data as { permalink?: string };
-      if (permalink) {
-        file.slugs = permalink.split('/').filter((s) => s !== '');
-      }
-    }
-  },
-};
 
 // See https://fumadocs.dev/docs/headless/source-api for more info
 export const source = loader({
   baseUrl: '/docs',
   source: docs.toFumadocsSource(),
-  plugins: [lucideIconsPlugin(), permalinkPlugin],
+  plugins: [lucideIconsPlugin()],
   icon(icon) {
     if (!icon) return;
 
     if (icon in icons) {
       return createElement(icons[icon as keyof typeof icons]);
     }
+  },
+  slugs(file) {
+    return file.data.permalink?.split('/');
   },
 });
 
